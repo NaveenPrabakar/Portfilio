f301cf835034233a904303caa266c0fc3bf1be40
Naveen Prabakar <123602587+NaveenPrabakar@users.noreply.github.com>
2025-05-26 11:31:53 -0500

    "updated security group"

--- Rag.py
+++ Rag.py
@@ -33,7 +33,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
+    allow_origins=["https://naveenprabakar.github.io"],  # Allows all origins
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
