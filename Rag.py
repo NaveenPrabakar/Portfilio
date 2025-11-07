5b547751f874bf3b8695b98cee1cd33b0f391802
Naveen Prabakar <123602587+NaveenPrabakar@users.noreply.github.com>
2025-11-06 20:09:08 -0600

    Fix import path for RecursiveCharacterTextSplitter

--- Rag.py
+++ Rag.py
@@ -8,7 +8,7 @@ from datetime import datetime
 import redis
 import json
 from uuid import uuid4
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader, TextLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.embeddings import OpenAIEmbeddings
